5c6a95e599f7364388958ed309ecc27034b2b4f1
zhangshuai5 <zhangshuai5@staff.weibo.com>
2021-03-23 16:26:23 +0800

    Initial Commit

---
+++ SSAlgorithmDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SSAlgorithmDemo
+//
+//  Created by zhangshuai5 on 2021/3/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
